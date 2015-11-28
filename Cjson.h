c84f520f922d29f33becfa36715cf513f9329a55
Graham Goudeau <grahamgoudeau@gmail.com>
2015-11-27 22:37:48 -0500

    Added copyCjson to header file

--- Cjson.h
+++ Cjson.h
@@ -19,6 +19,8 @@ Cjson createNewCjsonObject(void);
 
 Cjson createInitCjsonObject(char **keys, void **values, enum CJSON_TYPE *types, size_t numEntries);
 
+Cjson copyCJsonObject(CJson CjsonObj);
+
 bool doesKeyExist(Cjson CjsonObj, char *key);
 
 bool doesTypeMatch(Cjson CjsonObj, char *key, enum CJSON_TYPE type);
